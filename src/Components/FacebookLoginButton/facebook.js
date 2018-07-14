d579d4285755cb74f9cf7ca9db2831f94cf28c20
PiotrKowalczyk <contact@piotrkowalczyk.com>
2018-07-14 20:21:16 +0200

    blalbla

--- src/Components/FacebookLoginButton/facebook.js
+++ src/Components/FacebookLoginButton/facebook.js
@@ -1,67 +1,55 @@
 import React, {Component} from 'react';
 import FacebookLogin from 'react-facebook-login';
-
-
 class Facebook extends Component {
-
     state = {
         isLoggedIn: false,
         userId: '',
         name: '',
         email: '',
         picture: '',
+        movies: [],
+        friends: [],
     };
-
-    responseFacebook = response =>
-        // console.log(response);
-
-    this.setState({
-
-        isLoggedIn: true,
-        userID: response.userID,
-        name: response.name,
-        email: response.email,
-        picture: response.picture.data.url
-
-    });
-
+    responseFacebook = response => {
+        console.log(response);
+        this.setState({
+            isLoggedIn: true,
+            userID: response.userID,
+            name: response.name,
+            email: response.email,
+            picture: response.picture.data.url,
+            movies: response.movies.data,
+            friends: response.friends.data,
+        });
+    }
     componentClicked = () => console.log("clicked");
-
     render(){
-
         let fbContent;
-
         if(this.state.isLoggedIn) {
-
             fbContent = (
-
                 <div style = {{
                     width: '400px',
                     margin: 'auto',
                     background: '#f4f4f4',
                     padding: '20px'
-
                 }}>
-
-                <img src={this.state.picture} alt={this.state.name}/>
+                    <img src={this.state.picture} alt={this.state.name}/>
                     <h2>Witamy {this.state.name}</h2>
-                        Email: {this.state.email}
+                    Email: {this.state.email}
+                    Movies: {this.state.movies.map(movie => movie.name).join(', ')}
+                    Friends: {this.state.friends.map(friend => friend.name).join(', ')}
                 </div>
-
             )
-
         } else {
-
             fbContent =  (
                 <FacebookLogin
                     appId="202999163755971"
                     autoLoad={true}
-                    fields="name,email,picture"
+                    fields="name,email,picture,movies,friends"
                     onClick={this.componentClicked}
                     callback={this.responseFacebook} />
-                );
+            );
         }
-
         return (
             <div>
                 {fbContent}
@@ -69,6 +57,4 @@ class Facebook extends Component {
         );
     }
 }
-
 export default Facebook;
-
