0f4df72099b75eb40ebbc8cbda6b71aad9354c0f
thanh-thuy <thuy05061986@yahoo.com>
2018-07-14 13:25:37 +0200

    Login witch facebook.

--- src/Routes/Contacts/contact.js
+++ src/Routes/Contacts/contact.js
@@ -1,7 +1,7 @@
 import React, { PureComponent, Fragment } from 'react';
 import Header from '../../Components/Header/header.js';
 import Footer from '../../Components/Footer/footer.js';
-import Menu from '../../Components/navbar/menu.component.js';
+import Menu from '../../Components/Menu/menu.component.js';
 
 class Contact extends PureComponent {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, } from 'react-router-dom';
 import Home from './Routes/home.js';
 import Contact from "./Routes/Contacts/contact";
 import Dashboard from "./Routes/Dashboard/dashboard";
 import Favorites from "./Routes/Favorites/favorites";
 import Match from "./Routes/Match/match";
 import Settings from "./Routes/Settings/settings";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Navbar from "./Components/navbar/Navbar.component";
+import Login from "./Routes/Login/login";
+
+
 
 
 ReactDOM.render(
-    <MuiThemeProvider>
     <BrowserRouter>
         <Switch>
-            <Navbar/>
             <Route exact path='/' component={Home} />
             <Route path='/contact' component={Contact}/>
             <Route path='/dashboard' component={Dashboard} />
             <Route path='/favorites' component={Favorites} />
             <Route path='/match' component={Match} />
             <Route path='/settings' component={Settings} />
+            <Route path='/login' component={Login} />
+
         </Switch>
-    </BrowserRouter>
-    </MuiThemeProvider>, document.getElementById('root'));
+    </BrowserRouter>, document.getElementById('root'));
