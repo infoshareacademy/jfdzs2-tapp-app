2526d3dfb7c505d8ba35c01802abacc9b84b24c2
Piotr Kowalczyk <contact@piotrkowalczyk.com>
2018-07-14 14:10:55 +0200

    working version of facebook login

--- src/Routes/home.js
+++ src/Routes/home.js
@@ -2,28 +2,14 @@ import React, {PureComponent, Fragment} from 'react';
 import Header from '../Components/Header/header.js';
 import Footer from '../Components/Footer/footer.js';
 import Menu from '../Components/Menu/menu.component.js';
-import FacebookLoginButton from '../Components/FacebookLoginButton/FacebookLoginButton.js';
+import Facebook from '../Components/FacebookLoginButton/facebook.js';
 
 class Home extends PureComponent {
-
-    state = {
-        username: null
-    };
-
-    onFacebookLogin = (loginStatus, resultObject) => {
-        if (loginStatus === true) {
-            this.setState({
-                username: resultObject.user.name
-            });
-        } else {
-            alert('Facebook login error');
-        }
-    }
 
 
     render() {
 
-        const {username} = this.state;
+
 
 
         return (
@@ -31,25 +17,7 @@ class Home extends PureComponent {
                 <Header applicationName="Co nas łączy?"/>
                 <Menu/>
 
-                <div className="App">
-                    <header className="App-header">
-                        <h3 className="App-title">Yo ! Zaloguj się do aplikacji:</h3>
-                    </header>
-
-                    <div className="App-intro">
-                        {!username &&
-                        <div>
-
-                            <FacebookLoginButton onLogin={this.onFacebookLogin}>
-                                <button>Facebook</button>
-                            </FacebookLoginButton>
-                        </div>
-                        }
-                        {username &&
-                        <p>Welcome back, {username}</p>
-                        }
-                    </div>
-                </div>
+                <Facebook/>
 
                 <Footer/>
             </Fragment>
