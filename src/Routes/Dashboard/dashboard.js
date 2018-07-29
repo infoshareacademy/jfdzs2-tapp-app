bf383086ce1936cef459ccd82275e4f0647f4eb9
anna waluszko <ashiko85@gmail.com>
2018-07-29 22:50:32 +0200

    Small fixes

--- src/Routes/Dashboard/dashboard.js
+++ src/Routes/Dashboard/dashboard.js
@@ -1,7 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
-import Header from '../../Components/Header/header.js';
 import Footer from '../../Components/Footer/footer.js';
-import Menu from '../../Components/navbar/menu.component.js';
 
 class Dashboard extends PureComponent {
     render() {
